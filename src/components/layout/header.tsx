33e70ad5297dc63c27749906a52947eb258fa10a
chienhd <chienhd@liftsoft.vn>
2024-11-12 11:27:22 +0700

    feature(setting): add feature setting

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -9,15 +9,18 @@ import {
   MenubarTrigger,
 } from '@/components/ui/menubar';
 import { clientSessionToken } from '@/config/http.config';
+import { useRouter } from 'next/navigation';
 
 const Header = () => {
   const Logout = useLogout();
+
   const { data: userData } = useFetchUser();
+
+  const router = useRouter();
+
   const handleLogout = () => {
     Logout.mutate();
   };
-
-  console.log('clientSessionToken', clientSessionToken);
 
   return (
     <div className='fixed right-0 top-0'>
@@ -26,8 +29,12 @@ const Header = () => {
           <MenubarMenu>
             <MenubarTrigger>{userData && userData.data.email}</MenubarTrigger>
             <MenubarContent>
-              <MenubarItem>Profile</MenubarItem>
-              <MenubarItem>Setting</MenubarItem>
+              <MenubarItem onClick={() => router.push('/profile')}>
+                Profile
+              </MenubarItem>
+              <MenubarItem onClick={() => router.push('/setting')}>
+                Setting
+              </MenubarItem>
               <MenubarItem onClick={handleLogout} disabled={Logout.isPending}>
                 Đăng xuất
               </MenubarItem>
